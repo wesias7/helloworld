431ac6c3e4b5e63be9616982afbe3534d5bc7a79
wesias7 <wesias7@gmail.com>
2017-12-05 15:00:51 +0900

    token console.log
    token console.log

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,7 @@ router.get('/jwt/create', function(req, res, next){
     var secret = "wesias7";
     var payload = { email_address : "wesias7@gmail.com" };
     jwt.sign(payload, secret, { algorithm : 'HS256' }, function(err, token){
+        console.log(token);
         jwt.verify(token, secret, function(err, decoded){
             console.log(decoded);
             res.send(decoded);
